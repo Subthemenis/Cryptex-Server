e9d3de09091cbe51326e487c88ee06b68663ac2f
Subthemenis <hcleneveu@gmail.com>
2026-04-03 11:59:41 -0500

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,25 +1,14 @@
+import { wisp } from "@mercuryworkshop/wisp-js/server";
 import { createServer } from "http";
-import { WebSocketServer } from "ws";
+
+const server = createServer();
+
+// The wisp server handles all WebSocket upgrade requests
+server.on("upgrade", (req, socket, head) => {
+  wisp.routeRequest(req, socket, head);
+});
 
 const PORT = process.env.PORT || 3000;
-
-const server = createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("wisp server");
+server.listen(PORT, () => {
+  console.log(`Wisp server running on port ${PORT}`);
 });
-
-const wss = new WebSocketServer({ server });
-
-console.log(`Wisp server listening on port ${PORT}`);
-
-wss.on("connection", (ws, req) => {
-  console.log("Client connected");
-  ws.on("message", (data) => {
-    console.log("Received:", data);
-  });
-  ws.on("close", () => {
-    console.log("Client disconnected");
-  });
-});
-
-server.listen(PORT);
